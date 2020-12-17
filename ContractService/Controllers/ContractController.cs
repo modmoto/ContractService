34a3eb8342ff26d345b71b0dcdfc94421d29c4e9
Simon Heiss <simonheiss87@gmail.com>
2020-12-17 16:36:39 +0100

    fufu

--- ContractService/Controllers/ContractController.cs
+++ ContractService/Controllers/ContractController.cs
@@ -1,20 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace ContractService.Controllers
 {
